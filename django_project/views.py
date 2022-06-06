8624efe888b871ab80ed8c265c0ba444796b7ade
Danilo Borges <daniloborges.cg@gmail.com>
2022-06-06 22:41:16 +0000

    Template tag if elif e else

--- django_project/views.py
+++ django_project/views.py
@@ -1,8 +1,10 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
 
 def greeting(request):
   context = {
-    'greeting': 'Olá! Este é um exemplo do uso de templates. :)'
+    'greeting': 'Olá! Este é um exemplo do uso de templates. :)',
+    'today': datetime.now()
   }
   return render(request, 'greeting.html', context)
