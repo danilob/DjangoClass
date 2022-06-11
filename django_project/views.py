30ae12ffb6e03a6d9efe9d6803c66a7ba04e3f99
Danilo Borges <daniloborges.cg@gmail.com>
2022-06-11 00:06:20 +0000

    template for

--- django_project/views.py
+++ django_project/views.py
@@ -2,9 +2,12 @@
 from django.shortcuts import render
 from django.utils import timezone
 
+
 def greeting(request):
+  week_days = ['Seg','Ter','Qua','Qui','Sex','Sáb','Dom']
   context = {
     'greeting': 'Olá! Este é um exemplo do uso de templates. :)',
-    'today': timezone.now()
+    'today': timezone.now(),
+    'week_days': week_days 
   }
   return render(request, 'greeting.html', context)
