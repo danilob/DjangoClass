69a0ca2d761b5dcc53cd38873afd3b7fca4a57a5
Danilo Borges da Silva <danilo@Danilos-MacBook-Air.local>
2022-06-11 22:14:20 -0300

    adicionando classe a aplicação Django

--- scoreboard/admin.py
+++ scoreboard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from scoreboard.models import Team
+
+class TeamAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Team, TeamAdmin)
--- scoreboard/models.py
+++ scoreboard/models.py
@@ -1,3 +1,13 @@
+from unicodedata import name
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=100,primary_key=True)
+    coach = models.CharField(max_length=150)
+
+    class meta:
+        ordering = ["name"]
+        verbose_name_plural = "Teams"
+
+    def __str__(self):
+        return self.name.upper()
